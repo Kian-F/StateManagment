51e9ca3a33c9dca9d312233ec00efe91106f9e96
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-12-11 15:43:59 +1100

    movie

---
+++ src/MovieList.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+
+
+const MovieList =  () =>{
+    const [movies, setMovies] = useState([
+        {
+            name: 'Harry Potter',
+            price: '$10',
+            id: 2133
+        },
+        {
+            name: 'Game Of Thrones',
+            price: '$10',
+            id: 21383
+        },
+        {
+            name: 'Inception',
+            price: '$10',
+            id: 213873
+        }
+
+    ])
+    return(
+
+    );
+}
+
+export default MovieList
